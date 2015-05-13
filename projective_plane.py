2eee1a8dd52691908729a7678d9777a7b65c7b93
Quentin Honoré <quentinhonore3@gmail.com>
2015-05-13 15:27:15 +0200

    Modification: more examples and corrections

--- projective_plane.py
+++ projective_plane.py
@@ -3,42 +3,49 @@ def is_projective_plane(s):
     Test whether the incidence structure s is a projective plane or not
 
     INPUT:
+    
     - ''s'' -- incidence structure
 
     OUTPUT:
+    
     True/False bool
 
-    EXAMPLES:
-    ::
+    EXAMPLES::
+    
     sage: p=designs.projective_plane(4)
     sage: is_projective_plane(p)
     True
+
+    sage: p=designs.projective_plane(2)
+    sage: is_projective_plane(p)
+    True
+    sage: p._blocks
+    [[0, 1, 6], [0, 2, 4], [0, 3, 5], [1, 2, 5], [1, 3, 4], [2, 3, 6], [4, 5, 6]]
+    sage: a=[[0, 1, 5], [0, 2, 4], [0, 3, 5], [1, 2, 5], [1, 3, 4], [2, 3, 6], [4, 5, 6]]
+    sage: s=IncidenceStructure(a)
+    sage: is_projective_plane(s)
+    False
+
     ::
-    """
-
-
-    
+    """    
     m=s.incidence_matrix()
-    l=len(m.column(0))
-    c=len(m[0])
-
+    l=m.nrows()
+    c=m.ncols()
+    v=False
     if l!=c:
         return False
-
     p=sum(m[i,0] for i in range(l))
     if p**2-p+1!=l or p<3:
-        return false
-
-
+        return False
     else:
-
-        for (i,j,) in [(i,j) for i in range(l) for j in range(l) if i<j]:
-            if m[i]*m[j]!=1 or m.column(i)*m.column(j)!=1:
-                return False
-            if sum(m[x,j] for x in range(l))!=p:
-                return False
-        
-            if j<l-1:
+        for j in range(l):
+            for i in range(j):
+                if m[i]*m[j]!=1 or m.column(i)*m.column(j)!=1:
+                    return False
+                if sum(m[x,j] for x in range(l))!=p:
+                    return False
+        for j in range(l-1):
+            for i in range(j):
                 for k in range(l)[j+1:]:
                     b=1
                     for n in range(l):
@@ -46,12 +53,11 @@ def is_projective_plane(s):
                             b*=0
                     if b==1:
                         return True
-
-
-
     return False
 
-        
+
+
+    
 
 
             
