2891c1ee9a94e9212d209aee462868713755c860
Quentin Honoré <quentinhonore3@gmail.com>
2015-05-13 14:38:52 +0200

    Modification of minimal_blocking_set, works now, creation of concurrent_lines

--- arc.py
+++ arc.py
@@ -7,9 +7,11 @@ def arc(s):
     Return the arc which has the biggest cardinality in s
 
     INPUT:
+    
     - ''s'' -- incidence structure
 
     OUTPUT:
+    
     The list of the points from the arc which has the biggest cardinality in s.
 
     EXAMPLES:
@@ -48,17 +50,22 @@ def blocking_set(s):
     Return the blocking set which has the lowest cardinality in s
 
     INPUT:
+    
     - ''s'' -- incidence structure
 
     OUTPUT:
+    
     The list of the points from the minimal blocking set in s.
 
-    EXAMPLES:
-    ::
+    EXAMPLES::
+    
         sage: s=designs.projective_plane(3)
         sage: blocking_set(s)
         [1, 3, 5, 7, 10, 11]
 
+        sage: p=designs.projective_plane(2)
+        sage: blocking_set(p)
+        EmptySetError: There is no minimal blocking set in this incidence structure
     ::
     """
     
@@ -73,7 +80,7 @@ def blocking_set(s):
         r=p.get_values(b)
         return [i for (i,j) in r.items() if j==1]
     except MIPSolverException :
-        raise EmptySetError('There is no minimal blocking set in this incidence structure')
+        raise EmptySetError('There is no blocking set in this incidence structure')
        
 
 
@@ -88,29 +95,30 @@ def blocking_set(s):
 
 def is_minimal_blocking_set(b,s):
      r"""
-    Test if the blocking set b is a minimal blocking set in s
+    Test whether the blocking set b is a minimal blocking set in s or not
 
     INPUT:
+    
     - ''b'' -- blocking set
     - ''s'' -- incidence structure
 
     OUTPUT:
+    
     True/False bool
 
-    EXAMPLES:
-    ::
-        sage: p=designs.projective_plane(4)
-        sage: b=blocking_set(p)
-        sage: is_minimal_blocking_set(b,p)
-        True
+    EXAMPLES::
+    
+    sage: p=designs.projective_plane(4)
+    sage: b=blocking_set(p)
+    sage: is_minimal_blocking_set(b,p)
+    True
+
+
     ::
     """
-
-
-    m=s.incidence_matrix()
-    v=True
-    
-    for i in b:
+     m=s.incidence_matrix()
+     v=True
+     for i in b:
         v=False
         for j in range(len(s._blocks)):
             if m[i,j]==1 and sum(m[k,j] for k in b if k!=i)==0:
@@ -118,14 +126,51 @@ def is_minimal_blocking_set(b,s):
                 break
         if not v:
             break
-
-
-    return v
-
+     return v
 
 
 
 
+
+
+ 
+def concurrent_lines(p,s):
+    r"""
+    Return the lines that contains the point p in s.
+
+    INPUT:
+
+    - ''p'' -- point
+    - ''s'' -- incidence structure
+
+    OUTPUT:
+    
+    The list of lines that contains the point p.
+
+    EXAMPLES::
+    
+        sage: s=designs.projective_plane(4)
+        sage: concurrent_lines(10,s)
+        [[0, 5, 10, 15, 19],
+        [1, 7, 10, 12, 17],
+        [2, 6, 10, 14, 16],
+        [3, 4, 10, 13, 18],
+        [8, 9, 10, 11, 20]]                
+        sage: concurrent_lines(30,s)
+        []
+
+        sage: p=designs.projective_plane(2)
+        sage: concurrent_lines(4,p)
+        [[0, 2, 4], [1, 3, 4], [4, 5, 6]]
+        
+    ::
+    """
+    res=[]
+    for z in s.blocks():
+        if p in z:
+            res.append(z)
+    return res
+    
 
 
 
@@ -137,34 +182,31 @@ def minimal_blocking_set(s):
     Return the minimal blocking set which has the lowest cardinality in s
 
     INPUT:
+    
     - ''s'' -- incidence structure
 
     OUTPUT:
+    
     The list of the points from the minimal blocking set which has the lowest cardinality in s.
 
-    EXAMPLES:
-    ::
+    EXAMPLES::
+    
         sage: s=designs.projective_plane(4)
         sage: minimal_blocking_set(s)
         [0, 3, 5, 7, 14, 15, 20]
 
     ::
     """
-    
     p=MixedIntegerLinearProgram(maximization=False)
     b=p.new_variable(binary=True)
     p.set_objective(p.sum(b[v] for v in s._points))
-    n=0
-
-
+    for x in s._points:
+        p.add_constraint(p.sum(b[tuple(i)] for i in concurrent_lines(x,s))>=b[x])    
     for i in s._blocks:
         p.add_constraint(p.sum(b[k] for k in i)>=1)
         p.add_constraint(p.sum(b[k] for k in i)<=len(i)-1)
-        
-        for j in i:
-            p.add_constraint(b[j]-p.sum(b[k] for k in i if k!=j)<=b[j,n])
-            p.add_constraint(b[j,n]<=b[j])
-        n+=1
+        p.add_constraint(b[tuple(i)]>=2-p.sum(b[k] for k in i))
+        p.add_constraint(1+len(i)*(1-b[tuple(i)])>=p.sum(b[k] for k in i))     
     try:
         p.solve()
     except MIPSolverException :
