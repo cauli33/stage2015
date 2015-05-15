33c0e4a113bf8a9d04b99b2a289f9bdf1191537a
Quentin Honoré <quentinhonore3@gmail.com>
2015-05-15 14:09:50 +0200

    Deleted useless verifications, add examples

--- projective_plane.py
+++ projective_plane.py
@@ -17,6 +17,9 @@ def is_projective_plane(s,verbose=False):
     sage: p=designs.projective_plane(4)
     sage: is_projective_plane(p)
     True
+    sage: is_projective_plane(p,verbose=True)
+    True
+
 
     sage: p=designs.projective_plane(2)
     sage: is_projective_plane(p)
@@ -27,6 +30,35 @@ def is_projective_plane(s,verbose=False):
     sage: is_projective_plane(s,verbose=True)
     Given any two distinct lines, there must be exactly one point incident with both of them : concerns points 0 and 2
     False
+
+
+    sage: s=IncidenceStructure([[1,2,3],[2,3,5]])
+    sage: is_projective_plane(s,verbose=True)
+    There must be the same numbers of points and lines
+    False
+
+
+    sage: s=IncidenceStructure([[0,1],[1,2],[2,3],[0,3]])
+    sage: is_projective_plane(s,verbose=True)
+    There is less than 3 points in the first line
+    False
+
+    
+    sage: s=IncidenceStructure([[1,2,3],[4,5,6],[1,2,6],[4,5,3],[2,3,4],[2,5,6]])
+    sage: is_projective_plane(s,verbose=True)
+    if k is the number of points in every lines, the total number of points in a projective plane must be k^2-k+1
+    False
+
+
+    sage: p=designs.projective_plane(2)
+    sage: a=p.blocks()
+    sage: a[2].append(4)
+    sage: s=IncidenceStructure(a)
+    sage: is_projective_plane(s,verbose=True)
+    Lines must contain the same number of points : concerns lines 0 and 2
+    False
+
+    
 
     ::
     """
@@ -48,14 +80,15 @@ def is_projective_plane(s,verbose=False):
         if verbose:
             print "if k is the number of points in every lines, the total number of points in a projective plane must be k^2-k+1"
         return False
+    compt=1
     for i in b:
         # Check if every lines contains the same number of points
         if len(i)!=k:
             if verbose:
-                print ("Lines must contain the same number of points : concerns lines 0 and",i)
+                print "Lines must contain the same number of points : concerns lines 0 and",compt
             return False
+        compt+=1
     m=s.incidence_matrix()
-    f=False
     for j in range(np):
         for i in range(j):
             # Check the number of lines passing through points i and j
@@ -66,19 +99,7 @@ def is_projective_plane(s,verbose=False):
                 if verbose:
                     print 'Given any two distinct points, there must be exactly one line incident with both of them : concerns points',i,'and',j
                 return False
-            # Search a third point such that the only line incident with i and j is not with this one if not already found
-            if not f:
-                e=inter.pop()
-                if len(m.nonzero_positions_in_column(e))<np:
-                    f=True
-            # Check the number of intersections between lines i and j
-            if m.column(i)*m.column(j)!=1:
-                if verbose:
-                    print 'Given any two distinct lines, there must be exactly one point incident with both of them : concerns points',i,'and',j
-                return False
-    if not f and verbose:
-        print 'There are no three points such as no line is incident with every one'
-    return f
+    return True
 
 
     
