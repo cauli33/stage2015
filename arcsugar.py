77fd7baaeac99c30ebf7326e68911c6fd0ccedc8
Quentin Honoré <quentinhonore3@gmail.com>
2015-06-19 14:08:09 +0200

    Changed to the large definition of arcs

--- arcsugar.py
+++ arcsugar.py
@@ -1,22 +1,24 @@
-def arc(s, filename):
+def arc(b, s, filename):
+    ' b is for the bibd and s for the (s,n)-arc'
     f = open(filename, "w")
     f.write("; Arc \n")
-
-    for p in s._points:
+    for p in b._points:
         f.write("(int q_{} 0 1)\n".format(p))
     f.write("(int m 0 ")
-    l=str(len(s._points))
+    l=str(len(b._points))
     f.write(l)
     f.write(") \n")
     f.write("(objective maximize m)\n")
     f.write("(= m (+ ")
-    for p in s._points:
+    for p in b._points:
         f.write(" q_{}".format(p))
     f.write("))\n")       
-    for b in s._blocks:
+    for bl in b._blocks:
         f.write("(le (+")
-        for i in b:
+        for i in bl:
             f.write(" q_{}".format(i))
-        f.write(") 2)\n")
+        f.write(") ")
+        f.write(s)
+        f.write(")\n")
     f.write(";END")
     f.close()
